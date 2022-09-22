58968ffed585a3821d4bfb360dbbcb2dc27c5b9b
Alex-Cianfoni <112397905+Alex-Cianfoni@users.noreply.github.com>
2022-09-22 08:37:21 -0400

    noew

---
+++ Assignment 1/tester.py
@@ -0,0 +1,11 @@
+favfood= ["pizza", "Chicken", "pasta"]
+print(favfood)
+favfood[0] = "stake"
+print(favfood)
+favfood.insert(2,"bread")
+print(favfood)
+favfood.append("more stake")
+print(favfood)
+favfood.remove("bread")
+print(favfood)
+
