71427d189243a3e71a3411f6dcba4d1a85da3039
Alex-Cianfoni <112397905+Alex-Cianfoni@users.noreply.github.com>
2022-10-10 08:03:42 -0400

    shhh

--- Assignment 1/tester.py
+++ Assignment 1/tester.py
@@ -1,4 +1,8 @@
-endless = 4
-while (endless < 5):
-    print("I'm stuck in this loop!!!!")
-    endless += 1
+number = [21, 25, 44, 16, 14, 9, 14, 5, 37]
+if number > 20 and number < 35:
+    print("between 20 and 35")
+if number > 5 and number < 20:
+    print("between 5 and 20")
+if number < 35:
+    print("greater then 35 duh")
+else number > 5:
