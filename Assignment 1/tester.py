ea997f93daa3816ad7533c8a622ba6c8a35a8e7a
Alex-Cianfoni <112397905+Alex-Cianfoni@users.noreply.github.com>
2022-10-14 14:35:36 -0400

    ne wnew

--- Assignment 1/tester.py
+++ Assignment 1/tester.py
@@ -1,26 +1,14 @@
-# first of all, breathe
-# don't let this intimidate you. break it down a part at a time. You've done all of these separately and now we're just combining them.
-'''
-Instructions: Create a list with at least 8 numbers, and make sure you have a raandom dispersement between 0-50
-Create a loop (which loop would be best given we know how long we're going [the length of the list]?) that runs through the entire list
-Inside of your loop, have an if/elif/else that checks AT EACH INDEX OF THE LIST:
-if the number is greater than 35: print ("greater than 35")
-elif the number is between 20-35: print ("between 20-35")
-elif the number is between 5-20: print ("between 5 and 20")
-else (the number is less than 5)
-'''
 
+number = [21, 25, 44, 16, 14, 9, 14, 5, 37]
+for x in number:
+    print(x)
 
-def main():
-    number = [21, 25, 44, 16, 14, 9, 14, 5, 37]
+    if x >20 and x < 35:
+        print("between 20 and 35")
+    elif x >5 and x < 20:
+        print("between 5 and 20")
+    elif x >35:
+        print("greater then 35 duh")
+    elif x < 5:
+        print("the number is less than 5")
 
-
-if number > 20 and number < 35:
-    print("between 20 and 35")
-if number > 5 and number < 20:
-    print("between 5 and 20")
-if number < 35:
-    print("greater then 35 duh")
-else number > 5:
-
-main()
