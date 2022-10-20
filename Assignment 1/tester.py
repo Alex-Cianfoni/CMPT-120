28d401bf4a84ea6f50d20ef38bb4b66f3131fdad
Alex-Cianfoni <112397905+Alex-Cianfoni@users.noreply.github.com>
2022-10-20 08:11:34 -0400

    4

--- Assignment 1/tester.py
+++ Assignment 1/tester.py
@@ -1,14 +1,14 @@
+def printAlex():
+    print("Alex")
 
-number = [21, 25, 44, 16, 14, 9, 14, 5, 37]
-for x in number:
-    print(x)
 
-    if x >20 and x < 35:
-        print("between 20 and 35")
-    elif x >5 and x < 20:
-        print("between 5 and 20")
-    elif x >35:
-        print("greater then 35 duh")
-    elif x < 5:
-        print("the number is less than 5")
+
+def main():
+    printAlex()
+
+
+
+
+main()
+
 
